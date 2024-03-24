eb431d6f15b68eadf6b902f64980fe3b7b478441
Agnese Kapnere <akapne01@gmail.com>
2024-03-24 14:35:38 +0200

    Add entrypoint and process instruction function

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use borsh::{ BorshDeserialize, BorshSerialize };
+use solana_program::{
+    account_info::{ AccountInfo },
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    pubkey::Pubkey,
+};
+
+/// Define the type of state stored in accounts/
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub struct GreetingAccount {
+    /// number of greetings
+    pub counter: u32,
+}
+
+// Declare and export the program's entrypoint
+entrypoint!(process_instruction);
+
+// Program entrypoint's implementation
+pub fn process_instruction(
+    program_id: &Pubkey, // Public key of the account the hello world program was loaded into
+    accounts: &[AccountInfo], // The account to say hello to
+    instruction_data: &[u8] // Ignored, all helloworld instructions are hellos
+) -> ProgramResult {
+    msg!("Hello World Rust program entrypoint");
+
+    Ok(())
 }
